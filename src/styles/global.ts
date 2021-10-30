a9350dac3fcf06212cf4cb18de1c8354fcbd0837
Luiz Tanure <letanure@gmail.com>
2021-10-30 07:49:03 -0300

    feat: add font Noto Sans, antialiasing

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,7 +1,25 @@
 import { createGlobalStyle } from 'styled-components'
 
 const GlobalStyles = createGlobalStyle`
+  /* noto-sans-regular - latin */
+  @font-face {
+    font-family: 'Noto Sans';
+    font-style: normal;
+    font-weight: 400;
+    font-display: swap;
+    src: local(''), url('/fonts/noto-sans-v21-latin-regular.woff2') format('woff2');
+  }
+  /* noto-sans-700 - latin */
+  @font-face {
+    font-family: 'Noto Sans';
+    font-style: normal;
+    font-weight: 700;
+    font-display: swap;
+    src: local(''), url('/fonts/noto-sans-v21-latin-700.woff2') format('woff2');
+  }
   * {
+    -moz-osx-font-smoothing: grayscale;
+    -webkit-font-smoothing: antialiased;
     box-sizing: border-box;
     margin: 0;
     padding: 0;
@@ -13,7 +31,7 @@ const GlobalStyles = createGlobalStyle`
     height: 100%;
   }
   body {
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    font-family: 'Noto Sans', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
   }
 `
 
