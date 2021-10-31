100c7ea3b61237a421c0f2171cbb3be850b63b7b
Luiz Tanure <letanure@gmail.com>
2021-10-30 21:56:39 -0300

    feat: add font design token

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -3,6 +3,37 @@ const grid = {
   gutter: '3.2rem'
 }
 
+const font = {
+  family: {
+    heading: {
+      local: true,
+      name: 'Varela Round',
+      src: '/fonts/varela-round-v13-latin-regular.woff2',
+      weight: 400
+    },
+    body: {
+      local: true,
+      name: 'Noto Sans',
+      src: '/fonts/noto-sans-v21-latin-regular.woff2',
+      weight: 400
+    }
+  },
+  size: {
+    xsmall: '1.2rem',
+    small: '1.4rem',
+    c400: '1.6rem',
+    large: '1.8rem',
+    xlarge: '2.0rem',
+    xxlarge: '2.8rem'
+  },
+  weight: {
+    light: 300,
+    normal: 400,
+    bold: 600
+  }
+}
+
 export default {
+  font,
   grid
 }
