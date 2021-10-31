9176a4d30b1289fd34429a37b7e3c289b8e45a16
Luiz Tanure <letanure@gmail.com>
2021-10-31 19:21:10 -0300

    test(logo): add basic tests

--- src/components/Logo/test.tsx
+++ src/components/Logo/test.tsx
@@ -1,13 +1,29 @@
-import { render, screen } from '@testing-library/react'
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
 
 import Logo from '.'
 
 describe('<Logo />', () => {
-  it('should render the logo', () => {
-    const { container } = render(<Logo onBackgroundColor="dark" text="Logo" />)
-
+  it('should render with default text', () => {
+    renderWithTheme(<Logo />)
     expect(screen.getByRole('heading', { name: /Logo/i })).toBeInTheDocument()
+  })
 
-    expect(container.firstChild).toMatchSnapshot()
+  it('should render the light version by default', () => {
+    renderWithTheme(<Logo />)
+    expect(
+      screen.getByRole('heading', { name: /Logo/i }).parentElement
+    ).toHaveStyle({
+      color: '#8943e3'
+    })
+  })
+
+  it('should render the light version when hasDarkBg is true', () => {
+    renderWithTheme(<Logo hasDarkBg={true} />)
+    expect(
+      screen.getByRole('heading', { name: /Logo/i }).parentElement
+    ).toHaveStyle({
+      color: '#be93f9'
+    })
   })
 })
